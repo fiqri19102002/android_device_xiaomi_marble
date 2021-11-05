3d889a82357eac26a1aa63e5fb2cc2fe0d020dca
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    marble: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
