c995388805454434d65122650c61d9c77813e1ee
Arian <arian.kulmer@web.de>
2024-08-29 20:48:29 +0200

    marble: Move sensors hals.conf back to vendor repo
    sensors.xiaomi is added by common tree now
    
    Change-Id: I541b656304bfde478966c84115d94bc989742149
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_PACKAGES += \
     WifiResMarble \
     WifiResMarbleCN \
 
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
