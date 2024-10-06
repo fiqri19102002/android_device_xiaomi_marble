57717ffcef898c4207f68415aa68fa6d0c26818c
Arian <arian.kulmer@web.de>
2024-10-06 12:50:26 +0200

    marble: Use extract-utils FIX_SONAME helper
    Change-Id: I84a38b3a253e0bafbd2f44c385dfee05157e01e9
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -99,25 +99,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i '/dolby/d' "${2}"
             ;;
-        vendor/lib64/hw/audio.primary.taro-marble.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-marble.so" "${2}"
-            ;;
-        vendor/lib64/hw/fingerprint.fpc.default.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --set-soname "fingerprint.fpc.default.so" "${2}"
-            ;;
-        vendor/lib64/hw/fingerprint.goodix.default.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix.default.so" "${2}"
-            ;;
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
-            ;;
-        vendor/lib64/libgrpc++_unsecure_prebuilt.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --set-soname "libgrpc++_unsecure_prebuilt.so" "${2}"
             ;;
         vendor/lib64/libkaraokepal.so)
             [ "$2" = "" ] && return 0
