5fc886387f216279351b2e035c568cf73d8ba4ab
Michael Bestas <mkbestas@lineageos.org>
2025-09-06 15:32:17 +0300

    marble: Remove trailing slashes from device.mk
    No other device in the org does this, and this makes us different for
    no real reason. Most of the newly added lines also didn't have slashes
    so just drop all of them.
    
    Change-Id: Iecd14df31bd744e8a0ad423be176ca082b74dd17
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderResMarbleCN \
     SystemUIResMarble \
     WifiResMarble \
-    WifiResMarbleCN \
+    WifiResMarbleCN
 
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
@@ -449,7 +449,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.class_main.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.class_main.sh \
